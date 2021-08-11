843f23413b4824907ff56637c67af19aec985bea
songhk8634 <87174062+songhk8634@users.noreply.github.com>
2021-08-11 23:10:23 +0900

    Python

---
+++ 5_week2/210512/210512 board.sql
@@ -0,0 +1,6 @@
+CREATE TABLE BOARD(SEQ NUMBER(10),
+                   TITLE VARCHAR2(50),
+                   WRITER VARCHAR2(50),
+                   CONTENTS VARCHAR2(50));
+                   
+select * from board;
